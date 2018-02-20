7400716ee54cb0b067fbe8f823cb7806649f80a9
fEll3 <MitraWebTechnlogy@gmail.com>
2018-02-20 18:44:14 +0000

    change greeting

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 // route handler
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "Filip" });
 });
 
 // use enviroment Port or 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-// http://localhost:5000/
+// localhost:5000/
